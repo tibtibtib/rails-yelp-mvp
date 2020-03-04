f9f15d93279c91932e8fdfd75480eb9f2236b4fd
Thibault Adet <thibault.adet@gmail.com>
2020-03-04 11:33:07 +0100

    model and routes created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0458559886',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0458559886',
+    category:     'italian'
+  },
+  {
+    name:         'La passarelle',
+    address:      'Marseille',
+    phone_number: '0458559886',
+    category:     'french'
+  },
+  {
+    name:         'Chez gaston',
+    address:      'Paris',
+    phone_number: '0458559886',
+    category:     'belgian'
+  },
+  {
+    name:         'Johnny Coffe',
+    address:      'New York',
+    phone_number: '0458559886',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
